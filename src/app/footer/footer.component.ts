b6030e77ff85ab5ad74343df25255ed2bc1534c8
Dibeo <115413042+Dibeo@users.noreply.github.com>
2025-05-26 19:10:53 +0200

    feat : add swal2 alert when quitting the page

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
-
+  confirmNavigation(url: string) {
+    Swal.fire({
+      title: 'Attention !',
+      text: 'Vous allez quitter la page. Voulez-vous continuer ?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Oui, continuer',
+      cancelButtonText: 'Annuler',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        window.open(url, '_blank');
+      }
+    });
+  }
 }
